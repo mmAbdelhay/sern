3e491c1c68ddecf7bbd65b45e49066915f0364df
abdelhay <mm.abdelhay2015@gmail.com>
2023-09-27 10:01:54 +0300

    adding ora and chalk

--- database/connection.js
+++ database/connection.js
@@ -1,26 +1,28 @@
 const Sequelize = require("sequelize");
+const ora = require("ora");
+const chalk = require("chalk");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-  process.env.DB_DATABASE_DEV,
-  process.env.DB_USERNAME_DEV,
-  process.env.DB_PASSWORD_DEV,
-  {
-    host: process.env.DB_HOST_DEV,
-    port: process.env.DB_PORT_DEV,
-    dialect: process.env.DB_DIALECT,
-  }
+    process.env.DB_DATABASE_DEV,
+    process.env.DB_USERNAME_DEV,
+    process.env.DB_PASSWORD_DEV,
+    {
+        host: process.env.DB_HOST_DEV,
+        port: process.env.DB_PORT_DEV,
+        dialect: process.env.DB_DIALECT,
+    }
 );
 
 const connectToDB = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-    return true;
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-    return false;
-  }
+    try {
+        await sequelize.authenticate();
+        console.log(chalk.greenBright('Connection has been established successfully.'))
+        return true;
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+        return false;
+    }
 };
 
-module.exports = { connectToDB: connectToDB };
+module.exports = {connectToDB: connectToDB};
--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const swaggerUi = require('swagger-ui-express');
 const path = require('path');
 const YAML = require('yamljs');
 const {mergedSwaggerDocument} = require("./swagger/swaggerConfigurations");
+const ora = require("ora");
+const {rateLimiterMiddleware} = require("./middlewares/rateLimiterMiddleware");
 
 const Reset = '\x1b[0m'; // Reset to default color
 const FgRed = '\x1b[31m'; // Red text
@@ -16,6 +18,8 @@ const FgYellow = '\x1b[33m'; // Yellow text
 
 const userPort = process.env.USER_PORT || 8080;
 const adminPort = process.env.ADMIN_PORT || 8000;
+
+const spinner = ora('Initializing server... ').start();
 
 const userApp = express();
 const adminApp = express();
@@ -32,6 +36,8 @@ userApp.use('/api-docs', swaggerUi.serve, swaggerUi.setup(mergedSwaggerDocument)
 
 userApp.use(cors());
 userApp.use(express.json({limit: "5mb"}));
+
+userApp.use(rateLimiterMiddleware);
 userApp.use(authMiddleware);
 
 require("./admin/adminPanel.js")(adminApp);
@@ -43,7 +49,14 @@ userApp.use((req, res, next) => {
     return res.status(404).json({error: `${req.url} not found in this server`});
 });
 
-DB.connectToDB();
+DB.connectToDB().then(() => {
+    adminApp.listen(adminPort, () => console.log(FgYellow + "Admin dashboard listening on port " + FgRed + adminPort + Reset));
+    userApp.listen(userPort, () => console.log(FgGreen + "Server Up on port " + FgRed + userPort + Reset));
+}).catch(() => {
+    console.log(FgRed + "Could not connect to database" + Reset);
+}).finally(() => {
+    spinner.stop();
+});
 
-adminApp.listen(adminPort, () => console.log(FgYellow + "Admin dashboard listening on port " + FgRed + adminPort + Reset));
-userApp.listen(userPort, () => console.log(FgGreen + "Server Up on port " + FgRed + userPort + Reset));
+module.exports = userApp;
+
