16a32b000a5df1508401fb9e67c337a463ffd548
abdelhay <mm.abdelhay2015@gmail.com>
2023-09-27 10:29:30 +0300

    fix typo error

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 module.exports.authMiddleware = (req, res, next) => {
-    const excludedRoutes = ['/Auth/login', '/Auth/register'];
+    const excludedRoutes = ['/auth/login', '/auth/register'];
     if (req && !excludedRoutes.includes(req.path)) {
         const userInfo = jwt.decode(req.headers.authorization?.split(" ")[1]);
         if (!userInfo) return res.status(403).json({message: "User not authorized"});
